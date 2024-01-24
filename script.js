d887eb5f17a95d323565c28dae9ba3f6cb5db18c
pelo-petek <pelda.petekkaya@std.yildiz.edu.tr>
2024-01-24 21:58:01 +0300

    pages added

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-function newPage(x){
-    <a href="x"></a>
-};
+function footer(){
+    return <footer class="footer">
+    <ul>
+        <li>rizgar company</li>
+        <li>created by Pelda Petekkaya</li>
+    </ul>
+</footer>;
+}
+const footerPart = document.getElementsByClassName("footer");
+const footerComponent = ReactDOM.createRoot(footerPart);
+footerComponent.render(<footer/>);
+
+const menu = document.getElementsByClassName("menu");
